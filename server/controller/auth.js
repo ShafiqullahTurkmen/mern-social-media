1c0be52659038224a4c4f1518c8f127e606038fd
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-08-04 07:50:48 +0300

    login route added

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -42,3 +42,27 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// LOGGING
+export const login = async (req, res) => {
+  try {
+    
+    const { email, password } = req.body;
+
+    if (!(email && password )) return res.status(400).json({ msg: "Missing parameters!"})
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ msg: `User does not exist.` });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: `Invalid Credentials` });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
+  }
+};
