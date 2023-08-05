a52b15e30f7cbab260d6bd3e3a6f0a97bfa135dc
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-08-05 08:31:29 +0300

    user route added

---
+++ server/controller/users.js
@@ -0,0 +1,3 @@
+export const getUser = async (req, res) => res.send("Get User") 
+export const getUserFriends = async (req, res) => res.send("getUserFriends") 
+export const addRemoveFriend = async (req, res) => res.send("addRemoveFriend") 
