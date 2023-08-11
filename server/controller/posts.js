0cf8705244102b7dc064ca57b1f0f786bdd42259
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-08-11 08:11:04 +0300

    posts route added

---
+++ server/controller/posts.js
@@ -0,0 +1,4 @@
+export const getFeedPosts = (req, res) => res.send("getFeedPosts")
+export const getUserPosts  = (req, res) => res.send("getUserPosts")
+export const likePost = (req, res) => res.send("likePost")
+export const createPost = (req, res) => res.send("likePost")
